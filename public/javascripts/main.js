5a3ce6b2ddfbd8bba260edc3a640794c90f39ba7
michael lechner <mlechner2@gmail.com>
2014-03-19 20:15:57 -0600

    Misc development work

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -228,6 +228,7 @@ $(document).ready(function(){
 		$('.tech-imgs img').css('margin-right', '15px');
 		$('.tech-imgs img').css('margin-top', '15px');
 		$('.port-item').css('margin-right', '0px');
+		$('.port-item').css('width', '100px');
 
 		$('.bottom-trigger').waypoint(function(direction){
 			$('#logo-box').fadeOut(300);
